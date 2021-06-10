b2f83cd7217d1814cbecb92f6d3cbe91ccc815e5
151ali <ali151yek@gmail.com>
2021-06-10 20:44:16 +0100

    create a Dummy UI

--- Grid/src/main/java/org/emp/gl/testtp/grid/MyGrid.java
+++ Grid/src/main/java/org/emp/gl/testtp/grid/MyGrid.java
@@ -3,6 +3,7 @@ import org.emp.gl.testtp.etatrobotlistener.EtatRobotListener;
 import org.emp.gl.testtp.robot.Robot;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.beans.PropertyChangeEvent;
 import org.emp.gl.testtp.etatrobotingrid.Position;
@@ -28,9 +29,10 @@ public class MyGrid extends javax.swing.JFrame implements EtatRobotListener {
 
     public MyGrid(Robot agent){
         this.agent = agent;
+
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
-
+        /*
         frame.getContentPane().add(panel);
 
 
@@ -43,7 +45,30 @@ public class MyGrid extends javax.swing.JFrame implements EtatRobotListener {
 
         frame.setSize(new Dimension(500, 500));
         frame.setVisible(true);
+*/
+        frame.
+        getContentPane().add(panel);
 
+        int row = MAX_X;
+        int col = MAX_Y;
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(500, 500));
+        //setBounds(100, 100, 500, 500);
+        panel.setLayout(new GridLayout(row, col));
+
+        JLabel[][] grid= new JLabel[row][col];
+        for (int i = 0; i < row; i++){
+            for (int j = 0; j < col; j++){
+                grid[i][j] = new JLabel();
+                grid[i][j].setBorder(new LineBorder(Color.BLACK));
+                //grid[i][j].setBackground(Color.black);
+                grid[i][j].setOpaque(true);
+                panel.add(grid[i][j]);
+            }
+        }
+        grid[0][0].setBackground(Color.red);
+        frame.setVisible(true);
     }
 
     @Override
